e7a6288abd30395b7e68ec6b047b290f105b8fbc
Alec <alec.ritzrow@gmail.com>
2024-04-22 04:20:45 -0800

    more tkinter

--- Py Code/tictactoe.py
+++ Py Code/tictactoe.py
@@ -1,11 +1,23 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
+import customtkinter
 
-root = Tk()
-root.title("tictactoe")
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+#appearance
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
 
-root.mainloop()
+#frame
+app = customtkinter.CTk()
+app.geometry("1080x720")
+app.title("tictactoe")
+
+#UI
+title = customtkinter.CTkLabel(app, text="TicTacToe")
+title.pack(padx=10, pady=10)
+image1 = Image.open("<C:/Users/aritz/Desktop/bs/1Pzpe.png>")
+test = ImageTk.PhotoImage(image1)
+label1 = tkinter.Label(image=test)
+label1.image = test
+
+app.mainloop()
