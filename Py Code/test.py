aeee16694634b3b2d70cf4daebd83dcab50d75be
Alec <alec.ritzrow@gmail.com>
2024-05-04 20:56:44 -0800

    fizzbuzz

--- Py Code/test.py
+++ Py Code/test.py
@@ -1,3 +1,14 @@
-operation = input()
-mylist = operation.split()
-print(mylist)
+
+def fizzbuzz():
+    i = 1    
+    while i < 101:
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 5 == 0:
+            print("buzz")
+        elif i % 3 == 0:
+            print("fizz")
+        else:
+            print(i)
+        i = i + 1
+fizzbuzz()
